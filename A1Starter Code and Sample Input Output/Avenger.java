cef0624357080c3e39b58898a8b4bffb0c786174
Velibor Tunic <vtuni546@mtroyal.ca>
2023-01-27 19:47:25 -0700

    Added all starter files provided

---
+++ A1Starter Code and Sample Input Output/Avenger.java
@@ -0,0 +1,12 @@
+
+public class Avenger implements Comparable <Avenger>{
+
+	// TODO: Implement the Avenger Class
+	
+	@Override
+	public int compareTo(Avenger o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
